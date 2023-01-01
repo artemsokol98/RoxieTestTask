984211dd35d2e48be6c85b11621d02e71a7b358e
Artem Sokolovskiy <artemsokol98@gmail.com>
2023-01-01 22:13:48 +0300

    Initial Commit

---
+++ RoxieTestTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RoxieTestTask
+//
+//  Created by Артем Соколовский on 01.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
